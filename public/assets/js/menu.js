5243189489fa84da1a08304af9d45ad21d182688
31431 <kitsada280@gmail.com>
2017-01-22 13:28:12 +0800

    Add API support

--- public/assets/js/menu.js
+++ public/assets/js/menu.js
@@ -108,7 +108,7 @@ function populateOwnerViewOrder() {
 function populateOtherOrders() {
   var chunk = "";
   var prevOrder = "";
-  $.getJSON("assets/orderlist.json", function(data) {
+  $.getJSON("/private/getList/"+customID, function(data) {
     var items = [];
     $.each(data, function(key, val) {
       if (val.name != $('#owner').text()) {
